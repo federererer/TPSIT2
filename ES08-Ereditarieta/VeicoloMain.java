70089980e6cf4f9f88e8c59d2ddfa1cc9a65bf73
federererer <federico.onesto2@gmail.com>
2025-01-28 08:37:12 +0000

    modifica esercizio

--- ES08-Ereditarieta/VeicoloMain.java
+++ ES08-Ereditarieta/VeicoloMain.java
@@ -1,9 +1,10 @@
 
 class Veicolo  {
     private String marca="Fiat";
-
+    private String modello="cinquecento";
     public void stampaDettagli() {
         System.out.println("La marca è " + marca);
+        System.out.println("Il modello è " + modello);
     }
 }
 
